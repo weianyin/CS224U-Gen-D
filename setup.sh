f0bf8fc8071b7d2a8d8e7d0392ef9cbedb9a56bc
Ubuntu <ubuntu@ip-172-31-10-7.us-west-2.compute.internal>
2023-06-07 04:21:17 +0000

    Setup files

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# conda create -n cs224u_env python=3.8
+# conda activate cs224u_env
+
+# conda install pytorch==1.8.0 torchvision torchaudio cudatoolkit=10.1 -c pytorch
+# pip install tqdm==4.58.0
+# pip install requests==2.25.1
+# pip install importlib-metadata==3.7.0
+# pip install filelock==3.0.12
+# pip install sklearn==0.0
+# pip install tokenizers==0.10.1
+
+pip install -r requirements.txt
